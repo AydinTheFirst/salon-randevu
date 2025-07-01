df3bbbfcc606805467a3ff00e87086b4c8b87788
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-07-01 16:36:54 +0300

    feat: update build paths and add environment setup scripts

--- apps/server/src/config/serve-static.config.ts
+++ apps/server/src/config/serve-static.config.ts
@@ -1,7 +1,7 @@
 import { ServeStaticModuleOptions } from '@nestjs/serve-static';
 import { join } from 'path';
 
-const clientDist = join(process.cwd(), '..', 'client', 'dist');
+const clientDist = join(process.cwd(), '..', 'client', 'build', 'client');
 
 export const serveStaticConfig: ServeStaticModuleOptions = {
   exclude: ['/api'],
